4ac58cc1a034531e097b12c96fe82d315e7db2cf
kavliukigor <kavliukigor@gmail.com>
2026-04-09 14:51:58 +0300

    feat: Update URL routing and enhance monthly challenge views

--- monthlu_challenges/challenges/views.py
+++ monthlu_challenges/challenges/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def january(request):
     return HttpResponse('This works!')
 
-def feb(request):
+def february(request):
     return HttpResponse('Challenge complete!')
+
+def march(request):
+    return HttpResponse('Learn Django for at least 20 minutes')
+
+def monthly(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'This works'
+    elif month == 'january':
+        challenge_text = 'Challenge complete!'
+    elif month == 'january':
+        challenge_text = 'Learn Django for at least 20 minutes'
+    else:
+        challenge_text='Not supported month'
+        return HttpResponseNotFound()
+    return HttpResponse(challenge_text)
