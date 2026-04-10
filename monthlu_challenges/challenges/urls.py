04cc73b1a9d1383190d52337d1470a3957ca7b64
kavliukigor <kavliukigor@gmail.com>
2026-04-10 11:13:26 +0300

    feat: Refactor monthly challenge views and update URL patterns for improved routing

--- monthlu_challenges/challenges/urls.py
+++ monthlu_challenges/challenges/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('<month>', views.monthly)
+    path('<int:month>', views.monthly),
+    path('<str:month>', views.monthly_challenge_by_name)
 ]
--- monthlu_challenges/challenges/views.py
+++ monthlu_challenges/challenges/views.py
@@ -1,26 +1,21 @@
-from django.http import HttpResponse,HttpResponseNotFound
+from calendar import month
+from django.http import HttpResponse,HttpResponseNotFound, HttpResponseRedirect, HttpResponseServerError
 from django.shortcuts import render
 
+challenges ={
+    'january':'This works',
+    'february':'Challenge complete!',
+    'march':'Learn Django for at least 20 minutes',
+    'may': 'BimBImbBImBIM',
+    'june':'BAmBamBAMBAm'
+}
 # Create your views here.
 
-def january(request):
-    return HttpResponse('This works!')
-
-def february(request):
-    return HttpResponse('Challenge complete!')
-
-def march(request):
-    return HttpResponse('Learn Django for at least 20 minutes')
+def monthly_challenge_by_name(request,month):
+    challenge_text=challenges[month]
+    return HttpResponse(challenge_text)
 
 def monthly(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = 'This works'
-    elif month == 'january':
-        challenge_text = 'Challenge complete!'
-    elif month == 'january':
-        challenge_text = 'Learn Django for at least 20 minutes'
-    else:
-        challenge_text='Not supported month'
-        return HttpResponseNotFound()
-    return HttpResponse(challenge_text)
+    months_list=list(challenges.keys())
+    redirect_month=months_list[month]
+    return HttpResponseRedirect('/challenges/' + redirect_month)
