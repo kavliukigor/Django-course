fcccc139e0c7c3bd0ca59c90f5b512baa8cc0d93
kavliukigor <kavliukigor@gmail.com>
2026-04-13 16:30:15 +0300

    feat: Enhance blog functionality by updating URL patterns, implementing posts view, and improving CSS styling for posts

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='starting_page'),
+    path('', views.index, name='starting-page'),
     path('posts', views.posts, name='posts-page'),
     path('posts/<slug:slug>', views.single_post,
-        name='posts-detail-page')
+        name='post-detail-page')
 ]
