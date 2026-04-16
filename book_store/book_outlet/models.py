32ae7ed2ef304abe2274689cba78250c6e443a6e
kavliukigor <kavliukigor@gmail.com>
2026-04-16 18:23:00 +0300

    feat: Add slug field to Book model and implement slug generation on save

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,9 +11,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1),MaxValueValidator(5)])
     author=models.CharField(null=True,max_length=100)
     is_best_selling_book=models.BooleanField(default=False)
+    slug=models.SlugField(default='',null=False)
 
     def get_absolute_url(self):
         return reverse("book-details", args={self.id})
     
+    def save(self, *args,**kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args,**kwargs)
+
     def __str__(self):
         return f'{self.title}({self.rating})'
