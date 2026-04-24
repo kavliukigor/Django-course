41e7d30da9ee8514b74b4ce0ba2e5aefa241c37f
kavliukigor <kavliukigor@gmail.com>
2026-04-24 15:41:04 +0300

    feat: Enhance Author model with full name method and update Book model to include related name for author association

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -9,12 +9,18 @@ class Author(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Book(models.Model):
     title=models.CharField(max_length=50)
     rating=models.IntegerField(
         validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author=models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author=models.ForeignKey(Author, on_delete=models.CASCADE, null=True,related_name='books')
     is_best_selling_book=models.BooleanField(default=False)
     slug=models.SlugField(default='',null=False,db_index=True)
 
@@ -24,6 +30,3 @@ def get_absolute_url(self):
 def save(self, *args,**kwargs):
     self.slug=slugify(self.title)
     super().save(*args,**kwargs)
-
-def __str__(self):
-    return f'{self.title}({self.rating})'
